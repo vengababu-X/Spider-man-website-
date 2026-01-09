419a70c98a7f54b6a0afceccabe198a397a031f7
Boligorla vengababu <boligorlavengababu@gmail.com>
2026-01-09 23:17:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ function reveal(x, y) {
   frontImage.style.clipPath = `circle(140px at ${x}px ${y}px)`;
 }
 
-/* MOUSE MOVE */
 document.addEventListener("mousemove", (e) => {
   reveal(e.clientX, e.clientY);
 });
 
-/* TOUCH MOVE (MOBILE) */
 document.addEventListener("touchmove", (e) => {
   const touch = e.touches[0];
   reveal(touch.clientX, touch.clientY);
