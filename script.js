873f0228f6ed76c1e396440a568f5b90c1681b13
Boligorla vengababu <boligorlavengababu@gmail.com>
2026-01-09 22:52:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const front = document.querySelector(".front");
+const frontImage = document.querySelector(".front");
 
-function move(x, y) {
-  front.style.setProperty("--x", `${x}px`);
-  front.style.setProperty("--y", `${y}px`);
+function reveal(x, y) {
+  frontImage.style.setProperty("--x", `${x}px`);
+  frontImage.style.setProperty("--y", `${y}px`);
+  frontImage.style.clipPath = `circle(140px at ${x}px ${y}px)`;
 }
 
-/* MOUSE */
+/* MOUSE MOVE */
 document.addEventListener("mousemove", (e) => {
-  move(e.clientX, e.clientY);
+  reveal(e.clientX, e.clientY);
 });
 
-/* TOUCH */
+/* TOUCH MOVE (MOBILE) */
 document.addEventListener("touchmove", (e) => {
-  const t = e.touches[0];
-  move(t.clientX, t.clientY);
+  const touch = e.touches[0];
+  reveal(touch.clientX, touch.clientY);
 });
