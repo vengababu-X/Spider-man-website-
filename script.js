d865526a6e31771909282095851310b136449575
Boligorla vengababu <boligorlavengababu@gmail.com>
2026-01-09 23:16:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ document.addEventListener("touchmove", (e) => {
   const touch = e.touches[0];
   reveal(touch.clientX, touch.clientY);
 });
+
+const cursor = document.querySelector(".custom-cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+
+document.addEventListener("touchmove", (e) => {
+  const t = e.touches[0];
+  cursor.style.left = t.clientX + "px";
+  cursor.style.top = t.clientY + "px";
+});
